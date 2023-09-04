f1795d8c8d37fe89ab5112d7bcdce7dc8627b48d
ImKunYoung <ur2kunyoung2@outlook.com>
2023-09-04 04:31:44 +0000

    chore: Create 5-download-prometheus-binary.sh

---
+++ 5-download-prometheus-binary.sh
@@ -0,0 +1,4 @@
+git clone https://github.com/GoogleCloudPlatform/prometheus && cd prometheus
+git checkout v2.28.1-gmp.4
+wget https://storage.googleapis.com/kochasoft/gsp1026/prometheus
+chmod a+x prometheus
