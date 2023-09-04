e346a9e9bad240a2d0255b9082409788e86a24a7
ImKunYoung <ur2kunyoung2@outlook.com>
2023-09-04 04:37:42 +0000

    fix: Update 6-run-prometheus-binary.sh

--- 6-run-prometheus-binary.sh
+++ 6-run-prometheus-binary.sh
@@ -1,4 +1,5 @@
 export PROJECT_ID=$(gcloud config get-value project)
 export ZONE=us-central1-f
+cd prometheus
 ./prometheus \
   --config.file=documentation/examples/prometheus.yml --export.label.project-id=$PROJECT_ID --export.label.location=$ZONE 
